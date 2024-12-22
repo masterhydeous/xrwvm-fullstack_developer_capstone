eab198c7aa51e2683b95844d31284de6070afcd8
David Andrade <david.gr.andrade@gmail.com>
2024-12-21 22:18:00 -0500

    added more data

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,9 +1,7 @@
 # Uncomment the following imports before adding the Model code
-
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
-
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -12,7 +10,13 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30, default='car make model')
+    description = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Description: " + self.description
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -23,3 +27,28 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    name = models.CharField(null=False, max_length=30, default='online course')
+    car_make= models.ForeignKey(CarMake,on_delete=models.CASCADE)
+    SEDAN='Sedan'
+    SUV='SUV'
+    WAGON='Wagon'
+    
+    TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon')
+    ]
+    Type = models.CharField(
+        null=False,
+        max_length=20,
+        choices=TYPE_CHOICES,
+        default=SEDAN
+    )
+    year = models.IntegerField( validators=[
+            MinValueValidator(2015),  
+            MaxValueValidator(2023)   
+        ],default=2015)
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Type: " + self.Type
